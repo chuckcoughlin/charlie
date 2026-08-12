4c7e7d31dfb559fba1477ad0de51e84dd89bcd74
chuckcoughlin <chuck_coughlin@comcast.net>
2026-08-11 21:54:55 -0600

    tools is a PyCharm project.

--- dev/bin/view_config.sh
+++ dev/bin/view_config.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # Print configuration parameters as read directly from the robot
+#
+echo "===== Installed packages ====="
+pip list
+
 export PYTHON_ROOT=$(CHARLIE_HOME)/src/charlie
 cd $PYTHON_ROOT
-python3 robot/inspect.py
+python3 tools/inspect.py
